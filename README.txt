8a00c434b8d18149a076bfdde2af78bd29a0a8f5
mariaclaranobrega <mariaclaranms@gmail.com>
2022-05-12 16:54:36 +0100

    geral

---
+++ README.txt
@@ -0,0 +1,3 @@
+Lista de tarefas.
+Primeira aplicação web a utilizar python, flask e sqlite.
+O objetivo é adicionar tarefas, atualizar e deletar.
---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request, redirect,url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/tarefas.db'
+db = SQLAlchemy(app)
+
+
+# Criação de tabela para armazenar tarefas
+class Tarefa(db.Model):
+    __tablename__ = "tarefas"
+    id = db.Column(db.Integer, primary_key=True)
+    conteudo = db.Column(db.String(200))
+    feita = db.Column(db.Boolean)
+
+
+db.create_all()
+db.session.commit()
+
+
+@app.route('/')
+def home():
+    todas_as_tarefas = Tarefa.query.all()
+    return render_template("index.html", lista_de_tarefas=todas_as_tarefas)
+
+
+@app.route('/criar-tarefa', methods=['POST'])
+def criar():
+    tarefa = Tarefa(conteudo=request.form['conteudo_tarefa'], feita=False)
+    db.session.add(tarefa)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+@app.route('/eliminar-tarefa/<id>')
+def eliminar(id):
+    tarefa=Tarefa.query.filter_by(id=int(id)).delete()
+    db.session.commit()
+    return redirect(url_for('home'))
+
+@app.route('/tarefa-feita/<id>')
+def feita(id):
+    tarefa = Tarefa.query.filter_by(id=int(id)).first()
+    tarefa.feita = not(tarefa.feita)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
